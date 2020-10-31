9ac544e779aa2462571e5464706d80a1d3533b9c
Jackson Huang <jacksonh201325@gmail.com>
2020-10-31 20:10:59 -0300

    added mixpanel refactored

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,56 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import OFlogo from "../static/images/dark-blue-logo.png"
-export class Navigation extends Component {
-  render() {
-    return (
-      <nav id="menu" className="navbar navbar-default">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-            >
-              <span className="sr-only">Toggle navigation</span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-            </button>
-            <img className="collapsed navbar-toggle" style={{ width: "55%", border: 'none' }} src={OFlogo} alt="Logo" />
-          </div>
-          <div
-            className="collapse navbar-collapse"
-            id="bs-example-navbar-collapse-1"
-          >
-            <img src={OFlogo} alt="Logo" />
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="#features" className="page-scroll">
-                  Solutions
-                </a>
-              </li>
-              <li>
-                <a href="#about" className="page-scroll">
-                  How it Works
-                </a>
-              </li>
-              <li>
-                <a href="#team" className="page-scroll">
-                  Team
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="page-scroll">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+import { MixpanelConsumer } from 'react-mixpanel';
+import { SOLUTIONS, HOWITWORKS, TEAM, CONTACT, TOGNAV } from '../data/constant'
+
+const Navigation = () => {
+  return (
+    <MixpanelConsumer>
+      {
+        mixpanel => (
+          <nav id="menu" className="navbar navbar-default">
+            <div className="container">
+              <div className="navbar-header">
+                <button
+                  type="button"
+                  className="navbar-toggle collapsed"
+                  data-toggle="collapse"
+                  data-target="#bs-example-navbar-collapse-1"
+                >
+                  <span className="sr-only">{TOGNAV}</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <img className="collapsed navbar-toggle mobile-header" src={OFlogo} alt="Logo" />
+              </div>
+              <div
+                className="collapse navbar-collapse"
+                id="bs-example-navbar-collapse-1"
+              >
+                <img src={OFlogo} className="collapsed logo" alt="Logo" />
+                <ul className="nav navbar-nav navbar-right">
+                  {
+                    [SOLUTIONS, HOWITWORKS, TEAM, CONTACT].map(link => {
+                      return (
+                        <li>
+                          <a onClick={() => {
+                            mixpanel.track("Click", { link });
+                          }} href="#features" className="page-scroll">
+                            {link}
+                          </a>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </div>
+            </div>
+          </nav>
+        )
+      }
+    </MixpanelConsumer>
+  )
 }
 
 export default Navigation;
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -4,27 +4,38 @@ import ClemsonLogo from "../static/images/Clemson.svg"
 import LSULogo from "../static/images/LSU.svg"
 import OhioStaeLogo from "../static/images/Ohio State.svg"
 import RutgersLogo from "../static/images/Rutgers.svg"
+import { PROMGRAMS } from "../data/constant"
 
-const Services = () => {
+
+const Porgrams = () => {
+
   return (
     <div id="programs" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Top Programs are finding ways to connect</h2>
+          <h2>{PROMGRAMS.title}</h2>
         </div>
         <div className="row">
           <Grid container >
-            <Grid container item md={3} className="logos">
-              <img style={{ cursor: "pointer" }} onClick={() => window.open("https://clemsontigers.com/sports/football", "_blank")} src={ClemsonLogo} alt="Clemson Logo" />
+            <Grid container item xs={3} className="logos">
+              <Grid container item justify="center" alignItems="center" className="big-logo">
+                <img onClick={() => window.open("https://clemsontigers.com/sports/football", "_blank")} src={ClemsonLogo} alt="Clemson Logo" />
+              </Grid>
             </Grid>
-            <Grid container item md={3} className="logos">
-              <img style={{ cursor: "pointer" }} onClick={() => window.open("https://lsusports.net/sports/football", "_blank")} src={LSULogo} alt="LSU Logo" />
+            <Grid container item xs={3} className="logos">
+              <Grid container item justify="center" alignItems="center" className="small-logo">
+                <img onClick={() => window.open("https://lsusports.net/sports/football", "_blank")} src={LSULogo} alt="LSU Logo" />
+              </Grid>
             </Grid>
-            <Grid container item md={3} className="logos">
-              <img style={{ cursor: "pointer" }} onClick={() => window.open("https://ohiostatebuckeyes.com/sports/m-footbl/", "_blank")} src={OhioStaeLogo} alt="OhioState Logo" />
+            <Grid container item xs={3} className="logos">
+              <Grid container item justify="center" alignItems="center" className="small-logo">
+                <img onClick={() => window.open("https://ohiostatebuckeyes.com/sports/m-footbl/", "_blank")} src={OhioStaeLogo} alt="OhioState Logo" />
+              </Grid>
             </Grid>
-            <Grid container item md={3} className="logos">
-              <img style={{ cursor: "pointer" }} onClick={() => window.open("https://scarletknights.com/sports/football", "_blank")} src={RutgersLogo} alt="Rutgers Logo" />
+            <Grid container item xs={3} className="logos">
+              <Grid container item justify="center" alignItems="center" className="big-logo">
+                <img onClick={() => window.open("https://scarletknights.com/sports/football", "_blank")} src={RutgersLogo} alt="Rutgers Logo" />
+              </Grid>
             </Grid>
           </Grid>
         </div>
@@ -33,4 +44,4 @@ const Services = () => {
   );
 }
 
-export default Services;
+export default Porgrams;
